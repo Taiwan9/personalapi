026f6953b9a9795a52e244c420ea68627bb74f0c
Taiwan Marinho <clownser1994@gmail.com>
2021-10-26 15:45:36 -0300

    Create class MessageDTO e modifie class personController

--- src/main/java/one/digitalinnovation/personalapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personalapi/controller/PersonController.java
@@ -1,7 +1,12 @@
 package one.digitalinnovation.personalapi.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
+import one.digitalinnovation.personalapi.dto.MessageResponseDto;
+import one.digitalinnovation.personalapi.entity.Person;
+import one.digitalinnovation.personalapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    @GetMapping
-    public  String getBook(){
+    private PersonRepository personRepository;
+
+    @Autowired
+    public  PersonController(PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDto createPerson(@RequestBody Person person){
+        Person savedPerson = personRepository.save(person);
         return "API test";
     }
 }
